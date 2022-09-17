d730058777fd82b6c080f7e56423bda379021232
DmitryTruten <dmitriy2503a@gmail.com>
2022-09-17 16:15:58 +0300

    learned form validation

---
+++ HTML Learn/03-Forms/Form Validation/numCheck.js
@@ -0,0 +1,11 @@
+const check = document.getElementById('check');
+const guess = new URLSearchParams(window.location.search);
+const num = guess.get('guess');
+const randomNum = Math.floor(Math.random() * 10) + 1;
+const textRandomNum = randomNum.toString();
+
+if(num === textRandomNum){
+  check.innerHTML = 'You guessed correctly!';
+} else {
+  check.innerHTML = 'Try again...';
+}
